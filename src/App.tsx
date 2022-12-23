b067c4874bfbffadc7519ee138dbc7d4ce1e9e1a
Semvil <yurikh11@gmail.com>
2022-12-23 10:49:05 +0200

    les5

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import './App.css';
+import './App.scss';
 import React, {FC, useState, ChangeEvent} from 'react';
-import { Task } from './Interface';
+
 import ToDoTask from './toDoTask';
+import Button from '@mui/material/Button';
+import { v4 as uuidv4 } from "uuid";
+import { ListItem } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import TextField from '@mui/material/TextField'
+import { Task } from './Interface';
+
 
 const App: React.FC = () => {
 
   const [task, setTask] = useState<string>('');
   const [todoName, setTodoList] = useState<Task[]>([]);
-  
+
 
   function handleChange (event: ChangeEvent<HTMLInputElement>): void {
     if (event.target.name === "task") {
@@ -16,15 +23,15 @@ const App: React.FC = () => {
   };
 
   function addTask  (): void {
-    const newTask = { taskName: task };
-    setTodoList ( [...todoName, newTask] );
-    console.log(todoName);
+    const newTask = { taskName: task, id:uuidv4() };
+    setTodoList([...todoName, newTask]);
+    console.log(newTask);
+
   }
 
-  function removeTask(task:any): void{
-    const taskDelete = todoName;
-    taskDelete.splice(task);
-    setTodoList([...taskDelete])
+  function removeTask(id: any): void{
+    const removedArr = [...todoName].filter(todo => todo.id !== id);
+    setTodoList(removedArr);
   }
 
   return (
@@ -33,14 +40,17 @@ const App: React.FC = () => {
        <h1 className='title'>Plans for the day</h1>
        <h2 className='titleBlock'>tasks</h2>
       <div className='inputContainer'>
-        <input type = "text" name = 'task' onChange = {handleChange} value = {task} />
+        <TextField id="outlined-basic" defaultValue="Small" size="small" label="Outlined" variant="outlined" type="text" name='task' onChange={handleChange} value={task} />
       </div> 
-      <button onClick={addTask}> Add task </button>
-      <button onClick={removeTask}> Delete task </button>
+      <Button className='Addtask' variant="contained" color="success" onClick={addTask}>
+      Add task
+      </Button>
       <div className='toDotask'>
-      {todoName.map((task: Task) => {
-        return <ToDoTask task={task}/>;
-        })}
+        {todoName.map((task: Task) => {
+          return (
+            <><ToDoTask task={task} />
+              <Button variant="contained"  startIcon={<DeleteIcon />} onClick={() => removeTask(task.id)}>Delete</Button></>)
+      })}
       </div>
     </div>
 
@@ -48,6 +58,7 @@ const App: React.FC = () => {
 }
 
 export default App;
+
 
 
 
